d218aaa11232913e19af860f1e1fd19dfef8ca2a
Matheus Matsukawa <matheusmatsu@gmail.com>
2022-10-09 22:12:03 -0300

    try to fix cors

--- front/src/services/apiService.js
+++ front/src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const connection = axios.create({
-  baseURL: "http://hackaton-olx-prod-hackaton-olx-m6tiu4.mo2.mogenius.io/",
+  baseURL: "http://hackaton-olx-prod-hackaton-olx-m6tiu4.mo2.mogenius.io:80/",
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers": "Authorization",
@@ -12,7 +12,15 @@ const connection = axios.create({
 
 export const axiosPOST = async (URL, data) => {
   try {
-    const res = await connection.post(URL, data);
+    const res = await connection.post(URL, data, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Authorization",
+        "Access-Control-Allow-Methods":
+          "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+    });
     return res.data;
   } catch (error) {
     console.log(error);
